bdb32ce2b7a9344b0716c03fffefa85a6804196e
RZ-Huang <eilin0603@gmail.com>
2019-04-27 13:44:10 +0800

    week2 homework

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,33 @@
 function capitalize(str) {
-  return str;
+  const stringToArray = str.split('');
+  const headString = stringToArray.splice(0, 1)[0].toUpperCase();
+  const arrayToString = stringToArray.join('');
+  return headString + arrayToString;
 }
 
 console.log(capitalize('hello'));
+
+/* The other solutions
+
+function capitalize(str) {
+  const stringToArray = str.split('');
+  const headString = stringToArray.splice(0, 1)[0].toUpperCase();
+  let arrayToString = '';
+  for (const prop of stringToArray) {
+    arrayToString += prop;
+  }
+  return headString + arrayToString;
+}
+
+--------------------------------------------
+function capitalize(str) {
+  const cutString = str.split('');
+  const headString = cutString[0].toUpperCase();
+  let combineArrayString = '';
+  for (let i = 1; i < cutString.length; i++) {
+    combineArrayString += cutString[i];
+  }
+  return headString + combineArrayString;
+}
+
+*/
